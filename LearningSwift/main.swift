42a4d2f46852892bb6828656bf179f869f6c99a3
Kewal Kalsi <kkalsi7@gatech.edu>
2022-12-23 20:56:30 -0500

    Add basic variable info

--- LearningSwift/main.swift
+++ LearningSwift/main.swift
@@ -7,9 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: VARIABLES & CONSTANTS
+// Regular variables are initialized with the "var" keyword
+// Constants are initialied with the "let" keyword
 
-let label = "The width is "
-let width = 94
-let widthLabel = label + String(width)
-print(widthLabel)
+var myVar = 20 // Variable of type int
+let myConstant = 20 // Constant of type int - Can only be set once
+
+
+// MARK: Implicit vs Explicit Initialization
+// Implicit Initialization is based off of initial value
+let myInt = 20 // INT
+
+let myDouble = 20.0 // DOUBLE
+
+// To expliticly initialize something, use ": [type]" after variable name
+let explicitFloat: Float = 4 // EXPLICIT FLOAT OF VALUE 4
+
+// Values must be explicitly converted from one type to another
+let label = "The width is "             // STRING
+let width = 94                          // INT
+let widthLabel = label + String(width)  // STRING - because of String(width)
+//let widthLabelNoCast = label + width  // THROWS TYPE ERROR
+
+
+
+
